4c1beb99a014cbd7ebe15c84c3a7e24c906b64a2
jestebangl <juan.1701120500@gmail.com>
2017-12-11 13:48:34 -0500

    Cambio en el namespaces de los proyectos, y en nombre de los proyectos, vinculandolos al nombre de la prueba CubeSummation

---
+++ XpertGroup.CubeSummation.WebSite/App_Code/Global.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+using Castle.Windsor;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.MicroKernel.Lifestyle;
+using Castle.MicroKernel.Registration;
+
+namespace XpertGroup.CubeSummation.WebSite.App_Code
+{
+    public class Global : HttpApplication, IContainerAccessor
+    {
+
+        private static IWindsorContainer container;
+
+        public Global()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        IWindsorContainer IContainerAccessor.Container
+        {
+            get { return Container; }
+        }
+
+        public static IWindsorContainer Container
+        {
+            get
+            {
+                return container;
+            }
+            private set
+            {
+                container = value;
+            }
+        }
+
+        private void InitializeIoC()
+        {
+
+            var ioc = Ragolo.Core.IoC.IocHelper.Instance;
+            var container = ioc.GetContainer();
+
+            ioc.Install(new Services.DependencyResolution.Operator.OperatorInstaller());
+            ioc.Install(new Services.DependencyResolution.Orchestrator.OrchestratorInstaller());
+            container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
+            Container = container;
+
+        }
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            InitializeIoC();
+            // Code that runs on application startup
+        }
+
+    }
+}
